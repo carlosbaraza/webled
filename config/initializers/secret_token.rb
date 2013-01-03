827128b54da6656b71a8d47530c2141f050354bf
Carlos Baraza Haro <demon.xel@gmail.com>
2013-01-03 02:55:44 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webled::Application.config.secret_token = '636a6d421abdcc117f083f122f5758a5f6630bb6644f663f20c0fb527c943b47cd2079cb21c094dd695a3784896ff393e5d7df5f14fefc358da16a97849b1b29'
